e21d63a6a208132473f5cb63afc28f5be086d253
Andy <aeulmer74@gmail.com>
2024-03-08 15:03:50 -0500

    morgan, post request

--- server.js
+++ server.js
@@ -1,12 +1,21 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import morgan from 'morgan';
 dotenv.config();
 
 const app = express();
 
+//middleware
+app.use(morgan('dev'));
+app.use(express.json());
+
 //routes
 app.get('/', (req, res) => {
-	console.log('listening');
+	res.send('HELLO YOU');
+});
+
+app.post('/', (req, res) => {
+	res.json({ message: 'received', data: req.body });
 });
 
 app.listen(process.env.PORT, () => {
