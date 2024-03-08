21675ef503acbe4b30763445f8335effb4fba5ce
Andy <aeulmer74@gmail.com>
2024-03-08 15:48:19 -0500

    get all jobs, dummy data

--- server.js
+++ server.js
@@ -1,10 +1,17 @@
 import express from 'express';
 import * as dotenv from 'dotenv';
 import morgan from 'morgan';
+import { nanoid } from 'nanoid';
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 5100;
+
+//data
+let jobs = [
+	{ id: nanoid(), company: 'travelers', position: 'SWE 1' },
+	{ id: nanoid(), company: 'liberty', position: 'SWE 2' },
+];
 
 //middleware
 if (process.env.NODE_ENV === 'development') {
@@ -21,6 +28,11 @@ app.post('/', (req, res) => {
 	res.json({ message: 'received', data: req.body });
 });
 
+app.get('/api/v1/jobs', (req, res) => {
+	res.status(200).json({ jobs });
+});
+
+//LISTENING
 app.listen(port, () => {
 	console.log('listening on port', process.env.PORT);
 });
