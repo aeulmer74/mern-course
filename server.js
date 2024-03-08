2410cecaf5d77433c9fc4213d73464d6cf9a946a
Andy <aeulmer74@gmail.com>
2024-03-08 16:34:04 -0500

    crud and test collection setup

--- server.js
+++ server.js
@@ -20,15 +20,57 @@ if (process.env.NODE_ENV === 'development') {
 app.use(express.json());
 
 //routes
-app.get('/', (req, res) => {
-	res.send('HELLO YOU');
-});
-
 app.post('/', (req, res) => {
 	res.json({ message: 'received', data: req.body });
 });
 
 app.get('/api/v1/jobs', (req, res) => {
+	res.status(200).json({ jobs });
+});
+
+app.post('/api/v1/jobs', (req, res) => {
+	const { company, position } = req.body;
+	if (!company || !position) {
+		res.status(400).json({ message: 'Bad Request, please provide position and company name' });
+		return;
+	}
+	const newJob = { id: nanoid(), company, position };
+	jobs.push(newJob);
+	res.status(201).json({ message: 'new job added', newJob });
+});
+
+app.get('/api/v1/jobs/:jobId', (req, res) => {
+	const { jobId } = req.params;
+	const job = jobs.find((job) => job.id === jobId);
+	if (!job) {
+		return res.status(404).json({ message: 'not found' });
+	}
+	res.status(200).json({ job });
+});
+
+app.patch('/api/v1/jobs/:jobId', (req, res) => {
+	const { jobId } = req.params;
+	const { company, position } = req.body;
+	if (!company || !position) {
+		res.status(400).json({ message: 'Bad Request, please provide position and company name' });
+		return;
+	}
+	const job = jobs.find((job) => job.id === jobId);
+	if (!job) {
+		return res.status(404).json({ message: 'not found' });
+	}
+	job.company = company;
+	job.position = position;
+	res.status(200).json({ job });
+});
+
+app.delete('/api/v1/jobs/:jobId', (req, res) => {
+	const { jobId } = req.params;
+	const newJobs = jobs.filter((job) => job.id !== jobId);
+	if (!job) {
+		return res.status(404).json({ message: 'not found' });
+	}
+	jobs = [...newJobs];
 	res.status(200).json({ jobs });
 });
 
