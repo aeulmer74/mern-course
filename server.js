d12bf5fb1a6b01de7a52e97a2ed9c46288aa1ed5
Andy <aeulmer74@gmail.com>
2024-03-08 11:40:03 -0500

    initial server setup

--- server.js
+++ server.js
@@ -1,1 +1,14 @@
-console.log('Wow a server!');
+import express from 'express';
+import dotenv from 'dotenv';
+dotenv.config();
+
+const app = express();
+
+//routes
+app.get('/', (req, res) => {
+	console.log('listening');
+});
+
+app.listen(process.env.PORT, () => {
+	console.log('listening on port', process.env.PORT);
+});
