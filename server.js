80537223ebee65e543c31e4f21dd523e7b5a6609
Andy <aeulmer74@gmail.com>
2024-03-08 16:49:05 -0500

    error and not found handlers

--- server.js
+++ server.js
@@ -31,8 +31,9 @@ app.get('/api/v1/jobs', (req, res) => {
 app.post('/api/v1/jobs', (req, res) => {
 	const { company, position } = req.body;
 	if (!company || !position) {
-		res.status(400).json({ message: 'Bad Request, please provide position and company name' });
-		return;
+		return res
+			.status(400)
+			.json({ message: 'Bad Request, please provide position and company name' });
 	}
 	const newJob = { id: nanoid(), company, position };
 	jobs.push(newJob);
@@ -43,7 +44,8 @@ app.get('/api/v1/jobs/:jobId', (req, res) => {
 	const { jobId } = req.params;
 	const job = jobs.find((job) => job.id === jobId);
 	if (!job) {
-		return res.status(404).json({ message: 'not found' });
+		throw new Error('No job with matching ID found');
+		// return res.status(404).json({ message: 'not found' });
 	}
 	res.status(200).json({ job });
 });
@@ -52,12 +54,14 @@ app.patch('/api/v1/jobs/:jobId', (req, res) => {
 	const { jobId } = req.params;
 	const { company, position } = req.body;
 	if (!company || !position) {
-		res.status(400).json({ message: 'Bad Request, please provide position and company name' });
-		return;
+		return res
+			.status(400)
+			.json({ message: 'Bad Request, please provide position and company name' });
 	}
 	const job = jobs.find((job) => job.id === jobId);
 	if (!job) {
-		return res.status(404).json({ message: 'not found' });
+		throw new Error('No job with matching ID found');
+		// return res.status(404).json({ message: 'not found' });
 	}
 	job.company = company;
 	job.position = position;
@@ -67,11 +71,22 @@ app.patch('/api/v1/jobs/:jobId', (req, res) => {
 app.delete('/api/v1/jobs/:jobId', (req, res) => {
 	const { jobId } = req.params;
 	const newJobs = jobs.filter((job) => job.id !== jobId);
-	if (!job) {
-		return res.status(404).json({ message: 'not found' });
+	if (!newJobs) {
+		throw new Error('No job with matching ID found');
+		// return res.status(404).json({ message: 'not found' });
 	}
 	jobs = [...newJobs];
 	res.status(200).json({ jobs });
+});
+
+//404 handler
+app.use('*', (req, res) => {
+	res.status(404).json({ message: 'NOT FOUND' });
+});
+
+app.use((err, req, res, next) => {
+	console.log(err);
+	res.status(500).json({ message: 'Something exploded ' });
 });
 
 //LISTENING
