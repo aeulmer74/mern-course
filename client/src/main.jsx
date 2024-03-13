d48a87cce2243575c454c5b6fe4e06715e3cb4c3
Andy <aeulmer74@gmail.com>
2024-03-13 11:21:19 -0400

    setup custom axios and connect server and client

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,18 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import './index.css';
+import myAxios from './utils/customFetch.js';
+
+try {
+	const res = await myAxios.get('/test');
+	console.log(res);
+} catch (e) {
+	console.log(e);
+}
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+	<React.StrictMode>
+		<App />
+	</React.StrictMode>
+);
--- client/vite.config.js
+++ client/vite.config.js
@@ -6,5 +6,12 @@ export default defineConfig({
 	plugins: [react()],
 	server: {
 		open: true,
+		proxy: {
+			'/api': {
+				target: 'http://localhost:5100/api',
+				changeOrigin: true,
+				rewrite: (path) => path.replace(/^\/api/, ''),
+			},
+		},
 	},
 });
